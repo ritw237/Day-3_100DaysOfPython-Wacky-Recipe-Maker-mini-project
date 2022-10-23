12b5f63d5cc83c5818315bedbfacce96e32d77e2
Ritwik Srivastava <ritwikdbz@gmail.com>
2022-10-23 15:29:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+food = input("Any recipe that comes into your mind:")
+
+plant = input("Any plant that comes in to your mind:")
+
+cooking = input("What method of cooking you prefer: Baking, Frying etc?")
+
+Burned_food = "Ruined"
+
+Household_item = input("Enter a random household item:")
+
+print(cooking,food,"with",Burned_food,plant,"on a bed of",Household_item)
+
+
+
